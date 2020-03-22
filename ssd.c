2f64f7c55a95334356d06e9df9c8c961e1504f25
Creaf <52791960+furkan15963@users.noreply.github.com>
2020-03-22 11:27:01 +0300

    SevenSegmentDisplay_CommonCathode
    For PIC microcontroller
    0.56 inch Cathode Seven Segment Digit LED Display

--- ssd.c
+++ ssd.c
@@ -1,65 +1,56 @@
+// ISIKFurkan ->  creaph.com
+// 0.56 inch Cathode Seven Segment Digit LED Display
+// Pin Layout 
+// Pin1  -> Anode E        Pin6  -> Anode B
+// Pin2  -> Anode D        Pin7  -> Anode A
+// Pin3  -> VCC            Pin8  -> VCC
+// Pin4  -> Anode C        Pin9  -> Anode F
+// Pin5  -> Dot-H          Pin10 -> Anode G
+
 #use FIXED_IO( B_outputs=PIN_B7,PIN_B6,PIN_B5,PIN_B4,PIN_B3,PIN_B2,PIN_B1,PIN_B0 )
-#define G   PIN_B0
-#define B   PIN_B1
-#define C   PIN_B2
-#define D   PIN_B3
-#define E   PIN_B4
-#define F   PIN_B5
-#define A   PIN_B6
-#define H   PIN_B7   //nokta-(dot)
+#define G   PIN_B0   // Pin10
+#define F   PIN_B1   // Pin9
+#define A   PIN_B2   // Pin7
+#define B   PIN_B3   // Pin6
+#define C   PIN_B4   // Pin4
+#define D   PIN_B5   // Pin2
+#define E   PIN_B6   // Pin1
+#define H   PIN_B7   // Pin5 (dot)
 
 void ssd(x)
 {
-   if(x==0)
+   switch(x)
    {
-      output_B(0x7E);
-      break;
-   }
-   if(x==1)
-   {
-      output_B(0x18);
-      break;
-   }
-   if(x==2)
-   {
-      output_B(0x6D);
-      break;
-   }
-   if(x==3)
-   {
-      output_B(0x3D);
-      break;
-   }
-   if(x==4)
-   {
-      output_B(0x1B);
-      break;
-   }
-   if(x==5)
-   {
-      output_B(0x37);
-      break;
-   }
-   if(x==6)
-   {
-      output_B(0x77);
-      break;
-   }
-   if(x==7)
-   {
-      output_B(0x1C);
-      break;
-   }
-   if(x==8)
-   {
-      output_B(0x7F);
-      break;
-   }
-   if(x==9)
-   {
-      output_B(0x3F);
-      break;
+   
+      case 0:
+            output_B(0x18);
+            break;
+      case 1:
+            output_B(0x18);
+            break;
+      case 2:
+            output_B(0x6D);
+            break;
+      case 3:
+            output_B(0x3D);
+            break;
+      case 4:
+            output_B(0x1B);
+            break;
+      case 5:
+            output_B(0x37);
+            break; 
+      case 6:
+            output_B(0x77);
+            break;      
+      case 7:
+            output_B(0x1C);
+            break;       
+      case 8:
+            output_B(0x7F);
+            break;         
+      case 9:
+            output_B(0x3F);
+            break;
    }
 }
-
-
